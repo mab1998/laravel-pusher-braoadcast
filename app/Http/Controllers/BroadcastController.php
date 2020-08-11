aa6617e93161a3ae3a9cb216a709c683a72abed0
Your Name <you@example.com>
2020-08-11 11:13:33 +0200

    init

---
+++ app/Http/Controllers/BroadcastController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Events\StatusLiked;
+
+class BroadcastController extends Controller
+{
+
+    public function SendMessage(Request $request){
+        // return $request;
+
+        event(new StatusLiked($request->message));
+        return "Ok";
+
+    }
+		
+		
+		
+		}
